f977e359139e2f2315e35c44de55a446f24068a7
paulinaeb <pdespejo18@gmail.com>
2021-06-28 14:34:00 -0400

    front promedio y funcion fixed

--- scripts/promedio.js
+++ scripts/promedio.js
@@ -7,3 +7,11 @@ function promedio(lista){
     return sumaLista / lista.length;
 }
 
+function CalcularPromedio(){
+    const valor= document.getElementById("inputlista").value;
+    const arr = valor.split(','); 
+    for(let i=0; i<arr.length;i++){
+        arr[i]=parseInt(arr[i]);
+    }
+    document.getElementById("promedio").innerText= "El promedio de tu lista de numeros es: "+promedio(arr);
+}
