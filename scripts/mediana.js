e31249dcd3b23a85f8b40207d8fa8ac09050f0d3
paulinaeb <pdespejo18@gmail.com>
2021-06-28 14:43:14 -0400

    front mediana y funcion hechos

--- scripts/mediana.js
+++ scripts/mediana.js
@@ -3,7 +3,7 @@ function mediana(lista1){
     let lista=lista1.sort(function(a, b){return a - b});
     //obtener mitad del arreglo
     const mitad = parseInt(lista.length /2);
-    let mediana;
+    let valormediana;
     //verifico si es par o impar
     if((lista.length%2)===0){ 
         const valor1=lista[mitad];
@@ -13,4 +13,14 @@ function mediana(lista1){
     else{ 
         valormediana= lista[mitad];
     }
+    return valormediana;
+}
+ 
+function CalcularMediana(){
+    const valor= document.getElementById("inputlista").value;
+    const arr = valor.split(','); 
+    for(let i=0; i<arr.length;i++){
+        arr[i]=parseInt(arr[i]);
+    } 
+    document.getElementById("mediana").innerText= "La mediana de tu lista de numeros es: "+mediana(arr);
 }
